b7b37f7cf41f6719a83677b97ba600b8f918d951
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-02-17 19:43:03 -0500

    Agrega estilos y elimina archivo CSS base

--- src/App.vue
+++ src/App.vue
@@ -17,44 +17,42 @@
 </template>
 
 <style>
+.contenedor {
+  text-align: center;
+  margin-top: 50px;
+}
 
-  .contenedor {
-    text-align: center;
-    margin-top: 50px;
-  }
+h1 {
+  color: #42b983;
+}
 
-  h1 {
-    color: #42b983;
-  }
+h2 {
+  color: #42b983;
+}
 
-  h2 {
-    color: #42b983;
-  }
+img {
+  width: 100px;
+  height: 100px;
+}
 
-  img {
-    width: 100px;
-    height: 100px;
-  }
+button {
+  margin: 10px;
+  padding: 10px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 5px;
+}
 
-  button {
-    margin: 10px;
-    padding: 10px;
-    background-color: #42b983;
-    color: white;
-    border: none;
-    border-radius: 5px;
-  }
+button:hover {
+  cursor: pointer;
+}
 
-  button:hover {
-    cursor: pointer;
-  }
+button:active {
+  background-color: #3a9e7d;
+}
 
-  button:active {
-    background-color: #3a9e7d;
-  }
-
-  button:focus {
-    outline: none;
-  }
-
+button:focus {
+  outline: none;
+}
 </style>
