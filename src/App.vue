98ff771ad5a8f0c78796b16e45c51ac8f79ca25e
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-02-17 19:31:42 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+</script>
+
+<template>
+  <div class="contenedor">
+    <img alt="Vue logo" src="./assets/logo.svg">
+    <h1>Hola mundo desde VUE</h1>
+
+    <br>
+
+    <!-- Contador -->
+    <h2>Contador: </h2>
+    <button>Aumentar </button>
+    <button>Disminuir</button>
+    <button>Reiniciar</button>
+  </div>
+</template>
+
+<style>
+
+  .contenedor {
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  h1 {
+    color: #42b983;
+  }
+
+  h2 {
+    color: #42b983;
+  }
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  button {
+    margin: 10px;
+    padding: 10px;
+    background-color: #42b983;
+    color: white;
+    border: none;
+    border-radius: 5px;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  button:active {
+    background-color: #3a9e7d;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+</style>
